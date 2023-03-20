d1364644c3ffedd86ef03696876f054ad65bc9f9
skynettw <skynet.tw@gmail.com>
2023-03-20 14:48:36 +0800

    0320 third update

--- 0320b.py
+++ 0320b.py
@@ -2,9 +2,13 @@ f = open("data.txt", "r", encoding="utf-8")
 data = f.readlines()
 f.close()
 print(data)
+'''
 scores = list()
 for d in data:
     scores.append(int(d))
+'''
+scores = [int(d) for d in data]  # 串列生成式 list comprehensive
+
 print(scores)
 print("Max:", max(scores))
 print("Min:", min(scores))
---
+++ 0320c.py
@@ -0,0 +1,20 @@
+f = open("bodyinfo.txt", "r", encoding="utf-8")
+rawdata = f.readlines()
+f.close()
+
+data = [item.split(",") for item in rawdata]
+heights = [int(d[1]) for d in data]
+weights = [int(d[2]) for d in data]
+bmis = [ round(int(d[2]) / (int(d[1])/100)**2,2) for d in data]
+'''
+bmis = list()
+for d in data:
+    w = int(d[2])
+    h = int(d[1])
+    bmi = w / h **2 
+    bmis.append(bmi)
+'''
+print("本班最高是{}公分！".format(max(heights)))
+print("本班最矮是{}公分！".format(min(heights)))
+print("本班平均身高是{}公分！".format(sum(heights)/len(heights)))
+print("本班的平均BMI是{}".format(sum(bmis)/len(bmis)))
