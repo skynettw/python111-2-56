e9cfbe0c49bc746d3438dfdd8eef4809529c1037
skynettw <skynet.tw@gmail.com>
2023-03-27 15:23:55 +0800

    6th commit

--- nkust1112web56/mysite/views.py
+++ nkust1112web56/mysite/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 import requests, json
+from mysite import models           # 匯入 mysite 資料夾底下 models.py 中所有的類別
 
 def index(request):
     myname = "金城武"
@@ -23,4 +24,19 @@ def all_data(request):
     return HttpResponse(msg)
 
 def filtered_data(request):
+    # 先刪除所有的舊資料
+    models.HBicycleData.objects.all().delete()
+    # 先把所有的資料放到資料庫中，比照all_data()中的程式碼
+    url = "https://opendata.hccg.gov.tw/OpenDataFileHit.ashx?ID=48DEDBDAC3A31FC6&u=77DFE16E459DFCE3F5CEA2F931E333F7E23D5729EF83D5F20744125E844FB27044F9892E6F09372518441B3BB84260426ADE242A57DFB9E8C9A50C50134F4F47"
+    r = requests.get(url)
+    data = json.loads(r.text)
+    bicycle_data = data["retVal"]
+    for item in bicycle_data:
+        new_record = models.HBicycleData(
+            sna = item['sna'].split("_")[1],
+            sbi = int(item['sbi']),
+            tot = int(item['tot']))
+        new_record.save()
+    # 從資料表裡面過濾出我們想要的資料
+    data = models.HBicycleData.objects.filter(sbi__gte=10)
     return render(request, "filter.html", locals())
