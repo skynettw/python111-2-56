9b0ea820edf89da4630469cc68a89076601403c6
skynettw <skynet.tw@gmail.com>
2023-04-24 14:11:22 +0800

    0424 first commit

--- nkust1112web56/nkust1112web56/urls.py
+++ nkust1112web56/nkust1112web56/urls.py
@@ -4,7 +4,7 @@ from mysite import views   # 引入views.py中所有的函式
 
 urlpatterns = [
     path('', views.index),      # 設定執行首頁顯示的功能由index函式負責
-    path('nkustnews', views.nkustnews),
+    path('nkustnews/', views.nkustnews),
     path('all/', views.all_data),         #顯示所有的站台資訊
     path('filter/', views.filtered_data), #只顯示超過5台可用自行車的站台資訊
     path('admin/', admin.site.urls),
